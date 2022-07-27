28871b05773892fba5e1c8050538b1efcdd26dc1
jaredshedr <jaredsurfside@gmail.com>
2022-07-27 14:04:19 -0400

    Outlined initial get request to reviews

--- server-API/db.js
+++ server-API/db.js
@@ -13,12 +13,37 @@ pool.connect()
   .then(() => console.log('possssgres connected'))
   .catch((err) => console.log(err, 'posgres error'));
 
-pool.query('select * from characteristics_reviews', (err, res) => {
-  console.log(err, res.rows[0]);
-});
+let getReviews = function (queryParams, callback) {
+  console.log('in db layer', queryParams);
+  pool.query(`select * from reviews WHERE product_id = ${queryParams.product_id}`)
+      .then((res) => {
+        let reviewResponse = res.rows;
+        return reviewResponse
+      })
+      .then((reviewResponse) => {
+        let i = 0;
+        for (let review of reviewResponse) {
+          pool.query(`select * from reviews_photos WHERE review_id = ${review.id}`)
+            .then((res) => {
+              review.pictures = res.rows
+              i++
+              if (i >= reviewResponse.length) {
+                return reviewResponse
+              }
+            })
+            .then((finalResult) => {
+              if (finalResult !== undefined) {
+                callback(null, finalResult)
+              }
+            })
+            .catch((err) => console.log(err))
+        }
+      })
+      .catch((err) => console.log(err));
+}
 
 module.exports = {
-  pool,
+  pool, getReviews
 };
 
 // const {
@@ -29,3 +54,6 @@ module.exports = {
 // host: HOST1,
 // database: DB,
 // port: DB_PORT,
+
+
+// select * from reviews JOIN reviews_photos ON reviews.id = reviews_photos.review_id WHERE product_id = ${queryParams.product_id}
--- server-API/index.js
+++ server-API/index.js
@@ -1,8 +1,54 @@
 const express = require('express');
 require('dotenv').config();
-const db = require('./db');
+const { getReviews } = require('./db');
 
 const app = express();
+app.use(express.json());
+
+// get /reviews/
+// params - page default 1, count - default 5, sort, product_id
+// response 200 ok
+app.get('/reviews', (req, res) => {
+  let page = req.query.page || 1;
+  let count = req.query.count || 5;
+  let sort = req.query.sort || 'relevant';
+  let product_id = req.query.product_id || null;
+  if (product_id === null) {
+    res.status(422).send('invalid product_id provided');
+  }
+
+  let reviewParams = {product_id: product_id, page: page, sort: sort, count: count};
+  if (product_id !== null) {
+    getReviews(reviewParams, (err, result) => {
+      if (err) {
+        res.status(422).send(err);
+      } else {
+        // manipulate result further from here based on query params
+        for(let i = 0; i < result.length; i++) {
+          if (result[i].reported === true) {
+            result.splice(i, 1);
+          }
+        }
+        res.status(200).send(result);
+      }
+    })
+  }
+})
+
+// get /reviews/meta
+// params - product_id
+// response 200 ok
+
+// post /reviews
+// response 201 created
+
+// put reviews/:review_id/helpful
+// incrememet helpful
+// response 204 no content
+
+// put /reviews/:review_id/report
+// change reported to true
+// response 204 no content
 
 const port = 3030;
 app.listen(port, () => {
